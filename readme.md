309e1b150189545d6cad7140dc9a0654b51f6ec8
Alexandros Paterakis <paterakis.alexandros2000@gmail.com>
2024-05-01 00:24:36 +0300

    Added Guide & Files

---
+++ readme.md
@@ -0,0 +1,159 @@
+# Vitis AI Image Classification Guide
+
+This guide aims to help setting-up and running models on the Versal Platform VCK190.
+It contains the basic steps in order to convert a model to its final version for Versal without pruning or any optional steps. This guide mainly focuses on PyTorch
+
+# Table of Contents
+1. [Preparation: Model & Host](#{prep})
+2. [Vitis-AI 3.0](#vitis-ai)
+3. [Versal](#versal)
+
+## Preparation: Model & Host
+
+### Model & Dataset
+First and foremost, at least **60 - 100GB** are required for the docker container and the Vitis-AI repo.
+
+Any model can be applied if layers and operations are supported by Vitis-AI.\
+https://docs.amd.com/r/3.0-English/ug1414-vitis-ai/Currently-Supported-Operators
+
+For example:
+* SiLU layers must be changed to LeakyReLU with 0.1 slope.
+* Plus operations must be changed with FloatFunctional in case of Pytorch:
+    >out += xx ---> out = self.ff.add(out, xx)
+
+While datasets can be used with their respective loaders from PyTorch or Tensorflow or as .pkl files, they must be available as an Image Folder to run on versal without heavy code modifications.
+
+### Host
+Assuming you have the model ready lets move on the preparation of the host.
+
+1. Install [Docker](https://docs.docker.com/engine/install/): 
+2. Verify its installation:
+    * docker run hello-world <br />  
+    * docker --version
+
+3. Pull the [Vitis-AI container](https://xilinx.github.io/Vitis-AI/3.0/html/docs/install/install.html). Note: The VitisAI container is independent to the Vitis-AI 3.0 repo. 
+    > docker pull xilinx/vitis-ai-\<framework>-\<arch>:latest <br />
+    ``<framework>``: pytorch, tensorflow, tensorflow2\
+    ``<arch>``: cpu, rocm
+
+4. Now we are ready to pull the github repo:
+    > git clone --branch 3.0 https://github.com/Xilinx/Vitis-AI.git
+
+## Vitis-AI
+
+After downloading the repository you can start Vitis-AI:
+> ./docker_run.sh xilinx/vitis-ai-\<framework>-\<arch>:latest
+
+where ``<framework>`` and ``<arch>`` the chosen installed container.
+
+At this stage Vitis AI is running and you are ready to use its functions.\
+In this example we quantize and compile our pre-trained resnet20 model skipping any optional optimization steps.
+
+* Activate the corresponding conda environment:
+    > conda info --envs
+
+    In case of Pytorch:
+    > conda activate vitis-ai-pytorch
+
+* You are now ready to run your python script based on the provided example.
+
+    * Check /workspace/src/vai_q_pytorch/examples/ for effective examples.
+
+    * It is also suggested to move any script and files in that folder for everything to run as smooth as possible.
+
+Similarly to [<ins>resnet20_cifar_vai.py</ins>](resources/resnet20_cifar_vai.py) of this repo, in order to correctly quantize and compile a model you need 3 main things.
+
+1. The model loaded and modified with any conversions required.
+2. The dataset ready in order to run inference for the calibration step of the quantization.
+3. Any further modifications in normalization, image sizing etc.
+
+**Note**: The accuracy estimation of VitisAI does not reflect its actual performance whereas its resource estimations are spot on.
+
+[<ins>resnet20_minimal_vai</ins>](resources/resnet20_minimal_vai.py) provides an example of the bare minimum requirements for the quantization and compilation to run smoothly.
+
+### Running the script
+As observed in the parser of the files, each one can be run with quantization mode: float, calib and test. We skip float mode in this guide as it simply runs inference on the model as every other tool.
+
+- **calib** is a necessary step generating the VitisAI layer equivalent of the model along with its automatically generated bias correction. In this step we can use the same batch size as the floating point model however it should be noted that large values will require extreme resources exceeding 16GB RAM. If the program is killed during this step, simply reduce the batch size. In my tests it does not impact the performance of the model.
+
+**NOTE**: If an error occurs stating any problems accessing the model or finding "ModelName.py" simply rerun the command and everything is fine.
+
+- **test** is the final step where the model is prepared for the platform we want to apply it to. In this step, the batch size must be strictly 1. We also include arguments like --deploy in order to generate the .xmodel file which we transfer on Versal.
+
+
+In order to quantize and calibrate our model we run:\
+``python resnet20_cifar_vai.py --data_dir "dataset/CIFAR10" --model_dir "./" --batch_size 16 --target DPUCVDX8G_ISA3_C32B6 --quant_mode calib``
+
+In order to deploy our quantized model in its .xmodel form we run:\
+``python resnet20_cifar_vai.py --data_dir "dataset/CIFAR10" --model_dir "./" --batch_size 1 --target DPUCVDX8G_ISA3_C32B6 --quant_mode test --inspect --deploy``
+
+**Each Versal Board has a specific DPU id which must be used as the target. DPUCVDX8G_ISA3_C32B6 is for VCK190**
+
+### Finalizing Model
+
+After running these commands, a "quantize_result" folder should be created containing the VAI .py version of the model and the exported .xmodel file along with some other ones for compatibility.
+
+At this stage the .xmodel must be further processed by VitisAI Runtime in order to be utilized by the board.
+
+In order to achieve that we must first run:
+> /workspace/board_setup/vck190/host_cross_compiler_setup.sh
+
+and\
+``unset LD_LIBRARY_PATH``\
+``source $install_path/environment-setup-cortexa72-cortexa53-xilinx-linux``
+
+Which will resolve all the requirements and dependencies. 
+
+Do **<ins>NOT</ins>** run this before finishing with the model deployment as it will lead to unloading the PATH variable and requiring a docker restart.
+
+After the installation is done you can simply run:\
+``vai_c_xir -x quantize_result/CifarResNet_int.xmodel -a /opt/vitis_ai/compiler/arch/DPUCVDX8G/VCK190/arch.json -o ./ -n resnet20CIFAR``
+
+where -x takes the .xmodel generated from the script, -a is the corresponding architecture json for each board (**<ins>for vck190  DO NOT CHANGE</ins>**), -o the output folder and -n the name of the finalized file.
+
+The final file will be an isolated subgraph .xmodel  so make sure to give it a different name. If you want, you can see the differences between the previous and new .xmodel with [Netron](https://github.com/lutzroeder/netron)
+
+## Versal
+
+At this point the model is finalized and you can move on to the board.
+
+In [Board_files](resources/board/) we can find all the required files for inference on our VCK190 Board for a ResNet20 on CIFAR10.\
+The required files are broken down as followed:
+
+1. Compiled model  (.xmodel)
+2. Labels (for Image classification)  (cifar10_labels.dat)
+3. Dataset in the form of images and folders (dataset.tar)
+4. Bash scripts for output handling
+5. Main source files in order to run the model. (C++ files)
+
+The model is created from the previous process.
+
+Labels can be easily changed with any text editor.
+
+Do not add the whole dataset. Only the images you intend to test. In its current form the image name contains the correct label and its number as label_xxx.png. This can be changed to your preference by modifying [check_runtime_top5_cifar10.py](resources/board/model_src/code/src/check_runtime_top5_cifar10.py).
+
+Bash Scripts:
+
+* [run_src.sh](resources/board/run_src.sh) Simply runs the whole process
+* [build_cifar10_test.sh](resources/board/model_src/build_cifar10_test.sh) unpacks the images and moves them in a single folder for easier access. You can change this accordingly to your labels and preferences.
+* [cifar10_performance.sh](resources/board/model_src/cifar10_performance.sh) runs performance evaluations on the model for 1 to 6 DPU threads. There aren't much to change here except the number of threads and how the results are saved
+* [run_all_cifar10_target.sh](resources/board/model_src/run_all_cifar10_target.sh) is an all in one script preparing the directories, compiling the binaries, running inference and then cleaning up. If you make any changes here make sure you change the corresponding files too.
+* [Compilation arguments files](resources/board/model_src/code/) build_app.sh and build_get_dpu_fps.sh are the gcc compilation arguments for the corresponding sources files. It is not recommended to change these.
+* [Inference handler](resources/board/model_src/code/src/check_runtime_top5_cifar10.py) is responsible for the label checking and handling of inference results along with accuracy calculations.
+
+
+Main Files:
+
+* [Common](resources/board/common/) These are included headers and dependencies, it is not recommended to change them.
+* [Main](resources/board/model_src/code/src/main_int8.cc) is the main program which takes the model file and runs all actions. Here you should change the normalization arguments in lines 203 and 205 along with any other preprocessing steps you want to make. Also a change in line 256 may be required if the model is trained on [-1,1] range instead of [0,1].
+* [DPU](resources/board/model_src/code/src/get_dpu_fps.cc) is a simple program for the DPU in order to use multiple threads. There shouldn't be any changes required here.
+
+
+Having covered all files. You can easily run predictions on your model by connecting to the board and running:
+``run_src.sh vck190 my_model.xmodel``
+
+## Credits
+
+This work is based on the user guides of AMD and their examples on [GitHub](https://github.com/Xilinx/Vitis-AI.git) along with their suite of examples in [Vitis-AI-Tutorials](https://github.com/Xilinx/Vitis-AI-Tutorials/tree/3.0).
+
+The provided example contains heavily modified code and is not recommended to "copy-paste" without understanding its functionality.
---
+++ resources/resnet20_minimal_vai.py
@@ -0,0 +1,302 @@
+import os
+import re
+import sys
+import argparse
+import time
+import pdb
+import random
+from pytorch_nndct.apis import torch_quantizer
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+random.seed(0)
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
+
+from tqdm import tqdm
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    '--data_dir',
+    default="/path/to/imagenet/",
+    help='Data set directory, when quant_mode=calib, it is for calibration, while quant_mode=test it is for evaluation')
+parser.add_argument(
+    '--model_dir',
+    default="/path/to/trained_model/",
+    help='Trained model file path. Download pretrained model from the following url and put it in model_dir specified path: '
+)
+parser.add_argument(
+    '--config_file',
+    default=None,
+    help='quantization configuration file')
+parser.add_argument(
+    '--subset_len',
+    default=None,
+    type=int,
+    help='subset_len to evaluate model, using the whole validation dataset if it is not set')
+parser.add_argument(
+    '--batch_size',
+    default=32,
+    type=int,
+    help='input data batch size to evaluate model')
+parser.add_argument('--quant_mode', 
+    default='calib', 
+    choices=['float', 'calib', 'test'], 
+    help='quantization mode. 0: no quantization, evaluate float model, calib: quantize, test: evaluate quantized model')
+parser.add_argument('--fast_finetune', 
+    dest='fast_finetune',
+    action='store_true',
+    help='fast finetune model before calibration')
+parser.add_argument('--deploy', 
+    dest='deploy',
+    action='store_true',
+    help='export xmodel for deployment')
+parser.add_argument('--inspect', 
+    dest='inspect',
+    action='store_true',
+    help='inspect model')
+
+parser.add_argument('--target', 
+    dest='target',
+    nargs="?",
+    const="",
+    help='specify target device')
+
+args, _ = parser.parse_known_args()
+
+#--------------- MODEL DEFINITIONS ------------------------------------------------------------------
+
+from functools import partial
+from typing import Dict, Type, Any, Callable, Union, List, Optional
+from torch.ao.nn.quantized.modules.functional_modules import FloatFunctional
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+        self.ff = torch.nn.quantized.FloatFunctional()
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        #out = self.ff.add(out, identity)
+        out = self.relu(out)
+
+        return out
+
+
+class CifarResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=10):
+        super(CifarResNet, self).__init__()
+        self.inplanes = 16
+        self.conv1 = conv3x3(3, 16)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.layer1 = self._make_layer(block, 16, layers[0])
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(64 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+resnet20_model_cifar = CifarResNet(BasicBlock,[3]*3)
+
+#--------------- END MODEL DEFINITIONS ---------------------------------------
+
+
+def load_data(data_dir='dataset/imagenet',
+              batch_size=128,
+              subset_len=None,
+              sample_method='random',
+              distributed=False,
+              model_name='resnet20',
+              **kwargs):
+
+  #prepare data
+  # random.seed(12345)
+  valdir = data_dir + '/val'
+  normalize = transforms.Normalize(
+      mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
+  dataset = torchvision.datasets.ImageFolder(
+      valdir,
+      transforms.Compose([
+          #transforms.Resize(resize),
+          #transforms.CenterCrop(size),
+          transforms.ToTensor(),
+          normalize,
+      ]))
+  if subset_len:
+    assert subset_len <= len(dataset)
+    if sample_method == 'random':
+      dataset = torch.utils.data.Subset(
+          dataset, random.sample(range(0, len(dataset)), subset_len))
+    else:
+      dataset = torch.utils.data.Subset(dataset, list(range(subset_len)))
+  data_loader = torch.utils.data.DataLoader(
+      dataset, batch_size=batch_size, shuffle=False, **kwargs)
+  return data_loader
+
+
+def evaluate(model, val_loader):
+
+  model.eval()
+  model = model.to(device)
+  for iteraction, (images, labels) in tqdm(
+      enumerate(val_loader), total=len(val_loader)):
+    images = images.to(device)
+    labels = labels.to(device)
+    #pdb.set_trace()
+    outputs = model(images)
+
+
+def quantization(title='optimize',
+                 model_name='', 
+                 file_path=''): 
+
+  data_dir = args.data_dir
+  quant_mode = args.quant_mode
+  finetune = args.fast_finetune
+  deploy = args.deploy
+  batch_size = args.batch_size
+  subset_len = args.subset_len
+  inspect = args.inspect
+  config_file = args.config_file
+  target = args.target
+  if quant_mode != 'test' and deploy:
+    deploy = False
+    print(r'Warning: Exporting xmodel needs to be done in quantization test mode, turn off it in this running!')
+  if deploy and (batch_size != 1 or subset_len != 1):
+    print(r'Warning: Exporting xmodel needs batch size to be 1 and only 1 iteration of inference, change them automatically!')
+    batch_size = 1
+    subset_len = 1
+
+  model = resnet20_model_cifar.cpu()
+  model.load_state_dict(torch.load(file_path, map_location=torch.device('cpu')))
+  #change only the shape of the data
+  input = torch.randn([batch_size, 3, 32, 32])
+  ####################################################################################
+  # This function call will create a quantizer object and setup it. 
+  # Eager mode model code will be converted to graph model. 
+  # Quantization is not done here if it needs calibration.
+  quantizer = torch_quantizer(
+      quant_mode, model, (input), device=device, quant_config_file=config_file, target=target)
+
+  # Get the converted model to be quantized.
+  quant_model = quantizer.quant_model
+  #####################################################################################
+
+
+  val_loader, _ = load_data(
+      subset_len=subset_len,
+      batch_size=batch_size,
+      sample_method='random',
+      data_dir=data_dir,
+      model_name=model_name)
+   
+  #calibration
+  evaluate(quant_model, val_loader)
+
+
+  # handle quantization result
+  if quant_mode == 'calib':
+    # Exporting intermediate files will be used when quant_mode is 'test'. This is must.
+    quantizer.export_quant_config()
+  if deploy:
+    #quantizer.export_torch_script()
+    #quantizer.export_onnx_model()
+    quantizer.export_xmodel()
+
+
+if __name__ == '__main__':
+
+  model_name = 'resnet20_cifar'
+  file_path = os.path.join(args.model_dir, model_name + '.pkl')
+
+  feature_test = ' float model evaluation'
+  if args.quant_mode != 'float':
+    feature_test = ' quantization'
+    # force to merge BN with CONV for better quantization accuracy
+    args.optimize = 1
+    feature_test += ' with optimization'
+  else:
+    feature_test = ' float model evaluation'
+  title = model_name + feature_test
+
+  print("-------- Start {} test ".format(model_name))
+
+  # calibration or evaluation
+  quantization(
+      title=title,
+      model_name=model_name,
+      file_path=file_path)
+
+  print("-------- End of {} test ".format(model_name))
